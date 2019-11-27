88325a8d9d23b063c60c9a857e144a151e473b92
zhushimao123 <15235942622@163.com>
2019-11-27 17:20:44 +0800

    获取access_token

--- app/Http/Controllers/Admin/WeixinController.php
+++ app/Http/Controllers/Admin/WeixinController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
+
 class WeixinController extends Controller
 {
     //确认微信服务器
@@ -14,8 +16,18 @@ class WeixinController extends Controller
     //获取access_token
     public function token()
     {
-        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx48451c201710dbcd&secret=f583f90f3aed8ec33ae6dd30eceebe5f";
-        $response = file_get_contents($url);
-        var_dump($response);
+        $key = 'access_token';
+        $redis_token = Redis::get($key);
+        if(!$redis_token)
+        {
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx48451c201710dbcd&secret=f583f90f3aed8ec33ae6dd30eceebe5f";
+            $response = file_get_contents($url);
+            $arr = json_decode($response,true);
+            redis::set($key, $arr['access_token']); //存入access_token
+            redis::expire($key, 3600);
+        }else{
+            return $redis_token;
+        }
+
     }
 }
